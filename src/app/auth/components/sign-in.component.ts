f9527888d742ad80bacb89f76fb5df31e908a405
Mikel Severson <mkseve@gmail.com>
2016-12-12 20:02:48 -0600

    sign in swaps to new sign out button when authed

--- src/app/auth/components/sign-in.component.ts
+++ src/app/auth/components/sign-in.component.ts
@@ -6,12 +6,15 @@ import { AuthService } from '../services/auth-service';
     selector: 'sign-in',
     styleUrls: ['./sign-in.component.css'],
     template: `
-    <div class="sign-in">
+    <div class="sign-in" *ngIf="!auth.authenticated">
         <h1 class="sign-in__heading">Sign in</h1>
         <button class="sign-in__button" (click)="signInAnonymously()" type="button">Anonymously</button>
         <button class="sign-in__button" (click)="signInWithGithub()" type="button">GitHub</button>
         <button class="sign-in__button" (click)="signInWithGoogle()" type="button">Google</button>
         <button class="sign-in__button" (click)="signInWithTwitter()" type="button">Twitter</button>
+    </div>
+    <div class="sign-out" *ngIf="auth.authenticated">
+        <button class="sign-in__button" (click)="signOut()" type="button">Logout</button>
     </div>
     `
 })
@@ -37,6 +40,10 @@ export class SignInComponent {
   signInWithTwitter(): void {
     this.auth.signInWithTwitter()
       .then(() => this.postSignIn());
+  }
+
+  signOut(): void {
+      this.auth.signOut();
   }
 
   private postSignIn(): void {
